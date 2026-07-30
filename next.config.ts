515ffdf6d81f4960c156c848d71caac4c1b55a84
Abu Amar <abuamar.albadawi@gmail.com>
2026-07-30 17:35:09 +0700

    chore: remove @phosphor-icons/react from optimizePackageImports (uses SSR subpath)

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   experimental: {
-    optimizePackageImports: ['@phosphor-icons/react', '@cloudflare/kumo'],
+    optimizePackageImports: ['@cloudflare/kumo'],
   },
 }
 
